60ffc3d7dec0e1c884445a449c8dea2f52d9a17b
Joseph Ramos <joseph.ramos@taller.net.br>
2015-01-25 09:32:01 -0500

    Refactorig grunt tasks.

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -21,8 +21,8 @@ module.exports = function(grunt) {
 				expand: true,
 				cwd: './assets',
 				src: [
-          'styles/**/*.!(sass|scss)',
-          'js/**/*.'
+          'styles/**/*.css',
+          'js/**/*.js'
         ],
 				dest: '.tmp/public'
 			}]
