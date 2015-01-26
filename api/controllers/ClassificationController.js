cb283815bb08ea9ba4f4636d62a84ef30f23fdc9
Joseph Ramos <joseph.ramos@taller.net.br>
2015-01-25 23:58:42 -0500

    Generate api to classification.

---
+++ api/controllers/ClassificationController.js
@@ -0,0 +1,11 @@
+/**
+ * ClassificationController
+ *
+ * @description :: Server-side logic for managing classifications
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
