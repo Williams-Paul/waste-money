76cd333ed8a1b0bb7c48e72d489d7799d415ed48
Joseph Ramos <joseph.ramos@taller.net.br>
2015-02-15 14:33:17 -0500

    Added statistics end point.

--- api/controllers/ClassificationController.js
+++ api/controllers/ClassificationController.js
@@ -1,3 +1,4 @@
+
 /**
  * ClassificationController
  *
@@ -5,17 +6,58 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var Promise = require('bluebird'),
+    _ = require('lodash');
+
 module.exports = {
   getStatistics: function(req, res) {
 
-    Classification.find().then(function(classifications) {
+    var promises = [];
+    var statistics = [];
 
-      // Product.find().exec(function(err, products) {});
-      return res.json({
-        statistics: classifications
+    Classification.find().then(function(statistics) {
+
+      statistics.forEach(function(classification) {
+        var product = Product.find().where({
+          classification: classification.id
+        });
+
+        promises.push(product);
+      });
+
+      Promise.settle(promises).then(function(results) {
+
+        results.forEach(function(result, index) {
+          if(result.isFulfilled()) {
+            var result = result.value();
+
+            if(result.length > 0) {
+
+              // Add products to statistics
+              statistics[index].products = statistics[index].id == result[0].classification ?
+                result :
+                [];
+            }
+          }
+        });
+
+        // Get total cost of the products
+
+        statistics.forEach(function(classification, index) {
+          // Set total value
+          statistics[index].total = 0;
+
+          // Get total of waste by classification
+          (classification.products || []).forEach(function(product) {
+            statistics[index].total += parseFloat(product.price);
+          });
+        });
+
+        return res.json({
+          statistics: statistics
+        });
+
       });
     });
-
   }
 };
-
