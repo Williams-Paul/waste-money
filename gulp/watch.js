18b027d1c3ba9cf15fa6f06b97b081835308c297
Joseph Ramos <joseph.ramos@taller.net.br>
2015-02-21 08:48:55 -0500

    Nesting in directory files to watch task

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 
 var paths = {
-  scripts: 'assets/js/*.js',
-  css: 'assets/styles/*.css'
+  scripts: 'assets/js/*/**.js',
+  css: 'assets/styles/*/**.css'
 }
 
 gulp.task('watch', function() {
